e1c8d53b6cad967be7cfab6029f22c479e35e017
Denniskuerschner <krschner.dennis@yahoo.de>
2020-03-14 15:15:42 +0100

    Modulo

---
+++ com.Modulo/src/com/Modulo/geradeZahlen.java
@@ -0,0 +1,15 @@
+package com.Modulo;
+
+public class geradeZahlen {
+
+	public static void main(String[] args) {
+		double input = Math.random();
+		   
+		if (input % 2 == 0){
+	            System.out.println("gerade");
+	    } else {
+	            System.out.println("ungerade");
+	        }
+	}
+
+}
