ae7ad59ef1c97bd0b5b9930e984b0c17c8047295
Denniskuerschner <krschner.dennis@yahoo.de>
2020-03-14 15:23:35 +0100

    Substring

---
+++ com.Substring/src/com/Substring/letztesWort.java
@@ -0,0 +1,13 @@
+package com.Substring;
+
+public class letztesWort {
+
+	public static void main(String[] args) {
+		String test = "Hallo Welt"; 
+		
+		String lastWord = test.substring(test.lastIndexOf(" ")+1);
+	     
+		 System.out.println(lastWord);
+	}
+
+}
