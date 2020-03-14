8d72bd536b5bdb40a5dfb824f604cc24f668d1b1
Denniskuerschner <krschner.dennis@yahoo.de>
2020-03-14 11:42:00 +0100

    Der Start

---
+++ com.Switch/src/com/Switch/Switching.java
@@ -0,0 +1,39 @@
+package com.Switch;
+
+import java.util.Scanner;
+
+public class Switching {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Bitte geben Sie einen Monat ein: ");
+		String benutzerAuswahl = input.next();
+      
+        
+        switch(benutzerAuswahl){
+            case "Januar":
+            case "März":
+            case "Mai":
+            case "Juli":
+            case "August":
+            case "Oktober":
+            case "Dezember":
+                System.out.println("Der Monat hat 31 Tage");
+                break;
+            case "Februar":
+                System.out.println("Der Monat hat 28 Tage");
+                break;
+            case "April":
+            case "Juni":
+            case "September":
+            case "November":
+                System.out.println("Der Monat hat 30 Tage");
+                break;
+            default: 
+                System.out.println("default");
+        }
+        System.out.println("Fertig!");
+	}
+
+}
