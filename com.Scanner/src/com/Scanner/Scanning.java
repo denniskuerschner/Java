547989b40cb2a7d5978b53bb662eec016cdd1eba
denniskuerschner <56190619+denniskuerschner@users.noreply.github.com>
2020-03-14 11:50:26 +0100

    Update Scanning.java

--- com.Scanner/src/com/Scanner/Scanning.java
+++ com.Scanner/src/com/Scanner/Scanning.java
@@ -6,23 +6,16 @@ public class Scanning {
 
 	public static void main(String[] args) {
 		String firstName;
-        String lastName;
+        	String lastName;
         
         try (Scanner input = new Scanner (System.in)) {
 			System.out.println("Bitte gebe deinen Vornamen ein:");
 			firstName = input.next();
-			// Hier Vornamen einlesen!
 			
 			System.out.println("Bitte gebe deinen Nachnamen ein:");
 			lastName = input.next();
-			// Hier Nachnamen einlesen!
 		}
-        
         System.out.println("Welcome, " + firstName + " " + lastName);
-        // Hier Willkommens-Nachricht ausgeben.
-        // Beispielsweise soll für Max Mustermann ausgegeben werden: 
-        /** Welcome, Max Mustermann */
-
 	}
 
 }
