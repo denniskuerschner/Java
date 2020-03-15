a657e792fc52a9f5c119c58562fb10aa4a7cb933
Denniskuerschner <krschner.dennis@yahoo.de>
2020-03-15 14:41:20 +0100

    maxArray

---
+++ com.maxArray/src/com/maxArray/maxArray.java
@@ -0,0 +1,19 @@
+package com.maxArray;
+
+public class maxArray {
+
+	public static void main(String[] args) {
+		int arr[] = { 12, 55, 87, 99, 32 };
+		int max = 0;
+
+		for (int Element : arr) {
+			if (max < Element) {
+				max = Element;
+			}
+
+		}
+		System.out.println(max);
+
+	}
+
+}
